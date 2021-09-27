05b3544d56fcc1d6af2b0525102e9ae651c71dcc
Tammi <thjn40270@edu.ucl.dk>
2021-09-27 13:26:58 +0200

    Add project files.

---
+++ hydac/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hydac
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
